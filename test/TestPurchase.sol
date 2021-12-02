215a12ac026f3ad9e26dedf66e7f1462ee44bce8
aashitadutta <aashitadutta@gmail.com>
2021-12-02 13:56:25 +0530

    TestFileCorrected

---
+++ test/TestPurchase.sol
@@ -0,0 +1,39 @@
+pragma solidity >=0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Purchase.sol";
+
+contract TestPurchase{
+    // The address of the purchase contract to be Tested
+    Purchase purchases = Purchase(DeployedAddresses.Purchase());
+
+    //id of the wallpaper that will be used for  testing
+    uint expectedWallpaperId = 8;
+
+    //The expected purchaser of the wallpaper is this contract
+    address expectedPurchaser = address(this);
+
+    //testing the purchase function
+    function testUserCanPurchase() public {
+        uint returedId = purchases.purchase(expectedWallpaperId);
+
+        Assert.equal(returedId, expectedWallpaperId,"Purchase of the expected wallpaper should match what is returned.");
+    }
+
+    //testing retrival of all wallpaper owners
+    function testGetPurchaserAddressByWallpaperId()public {
+        address purchaser = purchases.purchasers(expectedWallpaperId);
+        Assert.equal(purchaser, expectedPurchaser,  "Owner of the expected wallpaper should be this contract");
+    }
+
+    //Testing retrival of all wallpaper owners
+    function testGetPurchaserAddressByWallpaperIdArray() public {
+        //Store purchasers in memory rather than contract's storage
+        address[16] memory purchasers = purchases.getPurchasers();
+
+        Assert.equal(purchasers[expectedWallpaperId], expectedPurchaser, "Owner of the expected wallpaper should be this contract");
+    }
+}
+
+
